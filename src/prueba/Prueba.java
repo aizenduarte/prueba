9b2d3ff26a8c730e1c7f97ff390203f661ca4779
edgardev <proyecto.medd@gmail.com>
2017-10-20 20:29:08 -0500

    modulo suma 20_10_2017 828
    subi modulo de suma

---
+++ src/prueba/Prueba.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+import prueba.aritmetica.NumA;
+
+
+/**
+ *
+ * @author SenaFontibon
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NumA numerito = new NumA();
+        System.out.println("sumando "+numerito.sumar(5, 5));
+    }
+    
+}
---
+++ src/prueba/aritmetica/NumA.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba.aritmetica;
+
+/**
+ *
+ * @author SenaFontibon
+ */
+public class NumA {
+
+    public int sumar(int a, int b) {
+        return a + b;
+
+    }
+
+}
